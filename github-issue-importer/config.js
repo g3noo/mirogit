2c1dcc341d877c7bf33f029f2cd3c0d8400ca9a9
Geno <romanelli.geno@gmail.com>
2020-10-05 08:45:39 -0700

    Updated Config and Tokens

--- github-issue-importer/config.js
+++ github-issue-importer/config.js
@@ -1,10 +1,11 @@
 module.exports = {
     github: {
-        token: 'github-token',
+        token: '39150e517a740a1250645077609df6be43f1cb22',
     },
     miro: {
-        token: 'miro-token',
-        boardId: 'board-id',
-        inboxFrameId: 'frame-id'
+        token: '951499e3-e6cb-4e6d-931b-4da5fcd8270d',
+        boardId: 'o9J_kjsmD1M=',
+        inboxFrameId: '3074457350376026064'
     }
 };
+
